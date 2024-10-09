a8f287f8fab244a33ef8165a327a08c67c7dae38
Denys Lygach <denisligach98@gmail.com>
2024-10-09 12:14:44 +0300

    Add footer

--- resources/views/livewire/footer.blade.php
+++ resources/views/livewire/footer.blade.php
@@ -1,3 +1,58 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="w-full h-full max-w-2xl px-6 lg:max-w-7xl mx-auto font-Poppins">
+    <div class="text-primaryWhite pt-[60px] pb-[100px]">
+        <div class="w-full h-0.5 bg-[#E1E1E1] rounded-full"></div>
+        <div class="grid grid-cols-[min-content_1fr] justify-items-stretch">
+            <div>
+                <svg class="h-[100px]">
+                    <use xlink:href="/icons.svg#logo-footer"></use>
+                </svg>
+                <h3>
+                    Your natural candle made for your home and for your wellness.
+                </h3>
+            </div>
+
+            <div class="flex justify-end">
+                <nav class="w-full lg:w-[80%] gap-4 grid grid-cols-3 pt-[50px] justify-self-end">
+                    <ul class="font-medium space-y-4 text-[#E1E1E1]">
+                        <li class="text-primaryGreen font-normal mb-6">Discovery</li>
+                        <li>
+                            <a href="/discovery/new-season">New season</a>
+                        </li>
+                        <li>
+                            <a href="/discovery/most-searched">Most searched</a>
+                        </li>
+                        <li>
+                            <a href="/discovery/most-sealed">Most selled</a>
+                        </li>
+                    </ul>
+
+                    <ul class="font-medium space-y-4 text-[#E1E1E1]">
+                        <li class="text-primaryGreen font-normal mb-6">About</li>
+                        <li>
+                            <a href="/discovery/new-season">Help</a>
+                        </li>
+                        <li>
+                            <a href="/discovery/most-searched">Shipping</a>
+                        </li>
+                        <li>
+                            <a href="/discovery/most-sealed">Affilate</a>
+                        </li>
+                    </ul>
+
+                    <ul class="font-medium space-y-4 text-[#E1E1E1]">
+                        <li class="text-primaryGreen font-normal mb-6">Info</li>
+                        <li>
+                            <a href="/discovery/new-season">Contact us</a>
+                        </li>
+                        <li>
+                            <a href="/discovery/most-searched">Privacy Polices</a>
+                        </li>
+                        <li>
+                            <a href="/discovery/most-sealed">Terms & Conditions</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
 </div>
