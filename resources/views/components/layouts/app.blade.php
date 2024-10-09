54406e55feccc5a3bf530377ef34bcd0ed61b9f0
Denys Lygach <denisligach98@gmail.com>
2024-10-09 12:14:04 +0300

    Update layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -12,21 +12,20 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     @vite('resources/css/app.css')
 </head>
-<body class="font-sans antialiased">
+<body class="font-sans antialiased h-screen w-full">
+    <object type="image/svg+xml" data="/icons.svg" class="hidden"></object>
+    <div class="mx-auto relative w-full h-full flex flex-col justify-between">
+        <div class="bg-primaryWhite">
+            <livewire:header/>
+        </div>
 
-<div>
-    <div>
-        <div class="mx-auto relative w-full max-w-2xl px-6 lg:max-w-7xl">
-            <object type="image/svg+xml" data="/icons.svg" class="hidden"></object>
-            <livewire:header></livewire:header>
+        <main class="bg-primaryWhite h-full">
+            {{ $slot }}
+        </main>
 
-            <main class="mt-6">
-                {{ $slot }}
-            </main>
-
-            <livewire:footer></livewire:footer>
+        <div class="bg-primaryBlack">
+            <livewire:footer/>
         </div>
     </div>
-</div>
 </body>
 </html>
