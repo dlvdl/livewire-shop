07026671e43748e2d96e7fb29b3fcf3d2607046a
Denys Lygach <denisligach98@gmail.com>
2024-10-11 20:53:09 +0300

    Woriking on product page

--- resources/views/pages/product-page.blade.php
+++ resources/views/pages/product-page.blade.php
@@ -3,7 +3,21 @@
 <?php $slot = ''?>
 
 @section('content')
-    <div class="h-full">
-        <h1>Product page</h1>
+    <div class="h-full text-primaryBlack max-w-2xl mt-16 lg:max-w-7xl mx-auto py-10">
+        <div class="grid grid-cols-2">
+            <div class="h-full flex flex-col">
+                <img src="/assets/product-mint-big.png"/>
+
+                <p class="font-medium font-Poppins text-2xl text-center mb-4 leading-6 tracking-tight">
+                    All hand-made with natural soy wax,
+                    Candleaf is made for your pleasure moments.
+                </p>
+
+                <p class="text-xl font-Roboto font-medium text-primaryGreen uppercase text-center">
+                    🚚 FREE SHIPPING
+                </p>
+            </div>
+            <div class="h-full flex flex-col"></div>
+        </div>
     </div>
 @endsection
