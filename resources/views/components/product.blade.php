d1d970a29d995dc94f6fe513eb6a2930cd2ae643
Denys Lygach <denisligach98@gmail.com>
2025-01-15 21:04:42 +0200

    Redirecting to product page from home page

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -2,7 +2,7 @@
     'product'
 ])
 
-<div class="w-full bg-primaryWhite drop-shadow cursor-pointer">
+<a href="{{ route('product', $product->id) }}" class="w-full bg-primaryWhite drop-shadow cursor-pointer">
     <div>
         <div class="w-full bg-[#F7F8FA] flex justify-center">
             <img class="h-[210px] w-[280px]" src="{{ asset($product->galleryImage->path) }}">
@@ -10,4 +10,4 @@
         <p class="text-xl font-Poppins text-primaryBlack font-medium pt-2 px-4">{{ $product->name }}</p>
         <p class="font-Roboto text-primaryGreen font-medium text-right px-4">{{ $product->price }}</p>
     </div>
-</div>
+</a>
