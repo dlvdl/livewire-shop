b2229a3466a1c86d34a467a9d59920b206d91648
Denys Lygach <denisligach98@gmail.com>
2025-02-28 12:10:10 +0200

    Responsive products section

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,1 +1,1 @@
-<a href="{{ $link }}" class="bg-primaryGreen text-primaryWhite text-xl rounded px-[44px] py-4">{{ $text }}</a>
+<a href="{{ $link }}" class="bg-primaryGreen text-primaryWhite text-center lg:text-xl rounded px-[44px] py-4">{{ $text }}</a>
--- resources/views/discovery-collection-section.blade.php
+++ resources/views/discovery-collection-section.blade.php
@@ -2,7 +2,7 @@
     <div class="h-full w-full flex justify-center items-center">
         <div class="bg-primaryWhite pt-[30px] pb-[60px] px-[94px] bg-opacity-80 rounded text-primaryBlack font-Poppins">
             <p class="text-center text-6xl mb-4">🌱</p>
-            <h1 class="text-center text-6xl mb-4">The nature candle</h1>
+            <h1 class="text-center text-4xl lg:text-6xl mb-4">The nature candle</h1>
             <p class="text-center text-lg max-w-[530px]">All handmade with natural soy wax, Candleaf is a companion for all your pleasure moments</p>
             <div class="flex justify-center mt-10">
                 @include('components.button',
