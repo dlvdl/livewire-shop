c6dfc9f60ca38f1d6812d77add39c40d683d88ce
Denys Lygach <denisligach98@gmail.com>
2025-03-09 19:53:46 +0200

    Responsive cart page table

--- resources/views/livewire/cart-product-table-row.blade.php
+++ resources/views/livewire/cart-product-table-row.blade.php
@@ -8,7 +8,7 @@
         }
     }"
     class="border-t border-b border-[#E5E5E5] py-6">
-    <td class="text-start py-6">
+    <td class="text-start py-6 hidden md:table-cell">
         <div class="flex">
             <div>
                 <img class="w-[160px] h-[130px]" src="{{ asset($item->product->galleryImage->url) }}">
@@ -23,8 +23,8 @@
             </div>
         </div>
     </td>
-    <td>{{ $item->product->price }}</td>
-    <td class="text-center" :style="{
+    <td class="hidden md:table-cell">{{ $item->product->price }}</td>
+    <td class="text-center hidden md:table-cell" :style="{
                         width: quantity > 99 ? '116px' :
                         quantity > 9 ? '100px' :
                         '75px'
@@ -35,5 +35,34 @@
             </div>
         </div>
     </td>
-    <td class="text-end">{{ $this->subtotal }}</td>
+    <td class="text-end hidden md:table-cell">{{ $this->subtotal }}</td>
+    <td colspan="4" class="text-end md:hidden">
+        <div class="w-full flex flex-col space-y-4 py-4 px-2">
+            <div class="flex justify-between">
+                <div class="border rounded">
+                    <img class="object-contain w-[100px]"
+                         src="{{ asset($item->product->galleryImage->url) }}">
+                </div>
+                <div>
+                    <h3 class="font-medium font-Poppins text-2xl mb-4">{{ $item->product->name }}</h3>
+                    <button wire:click="removeItem()">
+                <span class="text-primaryGreen underline text-lg font-Roboto">
+                    Remove
+                </span>
+                    </button>
+                </div>
+            </div>
+            <div class="flex justify-between">
+                <p>Price</p>
+                <p>{{ $item->product->price }}</p>
+            </div>
+            <div class="flex justify-between">
+                <p>Price</p>
+                <p>{{ $this->subtotal }}</p>
+            </div>
+            <div>
+                <x-quantity-selector x-model="quantity"/>
+            </div>
+        </div>
+    </td>
 </tr>
--- resources/views/livewire/pages/cart-page.blade.php
+++ resources/views/livewire/pages/cart-page.blade.php
@@ -13,15 +13,15 @@
     <div class="flex justify-center">
         <div class="w-full">
             @if(count($cartItems) > 0)
-                <div class="w-full hidden md:flex px-4">
+                <div class="w-full flex px-4">
                     <table class="w-full">
                         <thead>
-                        <tr class="font-Roboto font-medium">
-                            <td class="text-start py-4">Product</td>
-                            <td class="py-4">Price</td>
-                            <td class="text-center py-4">Quantity</td>
-                            <td class="text-end py-4">Total</td>
-                        </tr>
+                            <tr class="font-Roboto font-medium">
+                                <td class="text-start py-4 hidden md:table-cell">Product</td>
+                                <td class="py-4 hidden md:table-cell">Price</td>
+                                <td class="text-center py-4 hidden md:table-cell">Quantity</td>
+                                <td class="text-end py-4 hidden md:table-cell">Total</td>
+                            </tr>
                         </thead>
                         <tbody>
                         @foreach($cartItems as $cartItem)
@@ -30,13 +30,6 @@
                         </tbody>
                     </table>
                 </div>
-
-                <div class="w-full md:hidden flex flex-col justify-center gap-4 px-4 divide-y">
-                    @foreach($cartItems as $cartItem)
-                        <livewire:cart-product-card :item="$cartItem" :key="$cartItem->id . 'card'"/>
-                    @endforeach
-                </div>
-
                 <livewire:cart-product-total/>
             @endif
         </div>
